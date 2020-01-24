57fbd3278ccc965560b0d5d36e72c713456122ce
Bayo Ade <bayo.300@gmail.com>
2020-01-24 20:36:31 +0000

    random circle size

--- steiner.js
+++ steiner.js
@@ -1,7 +1,5 @@
 let canvas = document.querySelector('canvas');
 
-
-
 ranCol = function(r, g, b) {
     this.r = r;
     this.g = g;
@@ -27,38 +25,42 @@ ranCol2 = function(r, g, b) {
 
 }
 
-
-
 // canvas.style.backgroundColor = ranCol();
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 
 let c = canvas.getContext('2d');
+
 // c.fillRect(x, y, width, height)
 
 c.fillStyle = ranCol2();
 c.fillRect(100, 100, 100, 100);
 c.fillRect(200, 200, 100, 100);
 c.fillRect(300, 300, 100, 100);
+c.fillRect(400, 200, 100, 100);
+c.fillRect(500, 100, 100, 100);
+
+
 console.log(canvas);
 
 // line
-c.beginPath();
-c.moveTo(50, 300);
-c.lineTo(300, 100);
-c.lineTo(400, 100);
-c.lineTo(200, 50);
-c.lineTo(500, 100);
-c.lineTo(300, 100);
-c.strokeStyle = ranCol2();
-c.stroke();
+// c.beginPath();
+// c.moveTo(50, 300);
+// c.lineTo(300, 100);
+// c.lineTo(400, 100);
+// c.lineTo(200, 50);
+// c.lineTo(500, 100);
+// c.lineTo(300, 100);
+// c.strokeStyle = ranCol2();
+// c.stroke();
 
 for (let i = 0; i < 150; i++) {
     var x = Math.random() * window.innerWidth
     var y = Math.random() * window.innerHeight
+    let circSize = Math.floor(Math.random() * 50);
     c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI * 2, false);
+    c.arc(x, y, circSize, 0, Math.PI * 2, false);
     c.strokeStyle = ranCol2();
     c.stroke();
 }
